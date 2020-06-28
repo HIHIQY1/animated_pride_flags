c6422146cfd8727ee80851c1658fb95bf4fc2f13
HIHIQY1 <HIHIQY1+git@gmail.com>
2020-06-28 22:20:55 +0200

    Added anim 4: Asexual flag

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # animated_pride_flags
+
+<b>Done:</b>
+- [Anim 1](./anim1/): Rainbow
+  - [\[APNG\]](https://puu.sh/FRqAW/1b35ba0564.apng)
+- [Anim 2](./anim2/): Lesbian
+  - [\[APNG\]](https://puu.sh/FV8ki/7f58a50a73.apng)
+- [Anim 3](./anim3/): Transgender
+- [Anim 4](./anim4/): Asexual
+
+<b>To be done:</b>
+- Anim 5 - Scheduled for 2020-06-29
+- Anim 6 - Scheduled for 2020-06-30
+- Anim 7 - Scheduled for 2020-06-30
+
+<br/>
+
+## Rendering
+To render all individual frames:
+1. Clone this repo
+2. `cd` into the folder of the animation you want to render (`./anim#/render/`)
+3. Install all node modules by running `npm i` in that folder.
+4. Run the rendering script by running `node .` in that folder.
+5. All frames will be available at `./anim#/render/frames_SIZExFRAMERATE/` in the format of `#.png`.
+
+To encode all frames into an APNG:
+1. If you haven't already, install [FFmpeg](https://ffmpeg.org/) on your computer.
+2. `cd` into `./anim#/render/frames_SIZExFRAMERATE/`
+3. Run `ffmpeg -framerate FRAMERATE -i %d.png -plays 0 output.apng`
+4. Your output file will be at `./anim#/render/frames_SIZExFRAMERATE/output.apng`
---
+++ anim4/html/main.js
@@ -0,0 +1,163 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+let finalSize = 1024;
+
+let tStart = 0;
+let tEnd = 8;
+
+// cooleurs because we are cool 😎
+let cooleurs = [
+    "#000000",
+    "#a3a3a3",
+    "#ffffff",
+    "#800080"
+];
+
+canvas.width = finalSize;
+canvas.height = finalSize;
+tStart = Date.now();
+requestAnimationFrame(draw);
+
+function draw() {
+    let t = ((Date.now() - tStart) / 1000) % tEnd;
+
+    ctx.fillStyle = "#1a1a1a";
+    ctx.fillRect(0, 0, finalSize, finalSize);
+
+    ctx.lineCap = "round";
+
+    if (t < tEnd / 4) {
+        let theTimeIsNow = easeInOutCubic(t / (tEnd / 4));
+
+        ctx.save();
+        ctx.rotate(degToRad(theTimeIsNow * 90));
+
+        ctx.fillStyle = cooleurs[0];
+        ctx.fillRect(0, 0, finalSize / 2, finalSize / 4);
+        ctx.fillStyle = cooleurs[1];
+        ctx.fillRect(0, finalSize / 4, finalSize / 2, finalSize / 4);
+        ctx.restore();
+
+        ctx.save();
+        ctx.translate(finalSize, 0);
+        ctx.rotate(degToRad(theTimeIsNow * -90));
+        ctx.translate(-finalSize, 0);
+
+        ctx.fillStyle = cooleurs[0];
+        ctx.fillRect(finalSize / 2, 0, finalSize / 2, finalSize / 4);
+        ctx.fillStyle = cooleurs[1];
+        ctx.fillRect(finalSize / 2, finalSize / 4, finalSize / 2, finalSize / 4);
+        ctx.restore();
+
+        let lowerHalfOffsetY = finalSize / 2 * theTimeIsNow;
+
+        ctx.fillStyle = cooleurs[2];
+        ctx.fillRect(- finalSize / 2 * theTimeIsNow, finalSize / 2 + lowerHalfOffsetY, finalSize / 2, finalSize / 4);
+        ctx.fillStyle = cooleurs[3];
+        ctx.fillRect(- finalSize / 2 * theTimeIsNow, finalSize / 4 * 3 + lowerHalfOffsetY, finalSize / 2, finalSize / 4);
+
+        ctx.fillStyle = cooleurs[2];
+        ctx.fillRect(finalSize / 2 + finalSize / 2 * theTimeIsNow, finalSize / 2 + lowerHalfOffsetY, finalSize / 2, finalSize / 4);
+        ctx.fillStyle = cooleurs[3];
+        ctx.fillRect(finalSize / 2 + finalSize / 2 * theTimeIsNow, finalSize / 4 * 3 + lowerHalfOffsetY, finalSize / 2, finalSize / 4);
+    } else if (t < tEnd / 2) {
+        let theTimeIsNow = easeInOutCubic((t - tEnd / 4) / (tEnd / 4));
+
+        let maxSize = Math.sqrt((finalSize / 2) ** 2 / 2) * 2;
+
+        let hnow = maxSize / 4 * theTimeIsNow;
+
+        ctx.save();
+        ctx.translate(finalSize / 2, finalSize / 2);
+        ctx.rotate(degToRad(theTimeIsNow * 180 - 180));
+        ctx.translate(-finalSize / 2, -finalSize / 2);
+
+        ctx.fillStyle = cooleurs[0];
+        ctx.fillRect(finalSize / 2 - maxSize / 2, finalSize / 2 - hnow * 2, maxSize, hnow);
+
+        ctx.fillStyle = cooleurs[1];
+        ctx.fillRect(finalSize / 2 - maxSize / 2, finalSize / 2 - hnow, maxSize, hnow);
+
+        ctx.fillStyle = cooleurs[2];
+        ctx.fillRect(finalSize / 2 - maxSize / 2, finalSize / 2, maxSize, hnow);
+
+        ctx.fillStyle = cooleurs[3];
+        ctx.fillRect(finalSize / 2 - maxSize / 2, finalSize / 2 + hnow, maxSize, hnow);
+        ctx.restore();
+    } else if (t < tEnd / 4 * 3) {
+        let theTimeIsNow = easeInOutCubic((t - tEnd / 2) / (tEnd / 4));
+
+        let maxSize = Math.sqrt((finalSize / 2) ** 2 / 2) * 2;
+
+        ctx.save();
+        ctx.translate(finalSize / 2, finalSize / 2);
+        //ctx.rotate(degToRad(theTimeIsNow * 180 - 180));
+        ctx.translate(-finalSize / 2, -finalSize / 2);
+
+        ctx.fillStyle = cooleurs[1];
+        ctx.fillRect(finalSize / 2 - maxSize / 2, finalSize / 2 - maxSize / 4, maxSize, maxSize / 4);
+
+        ctx.fillStyle = cooleurs[2];
+        ctx.fillRect(finalSize / 2 - maxSize / 2, finalSize / 2, maxSize, maxSize / 4);
+
+        ctx.fillStyle = cooleurs[3];
+        ctx.fillRect(finalSize / 2 - maxSize / 2, finalSize / 2 + maxSize / 4, maxSize, maxSize / 4);
+
+        ctx.fillStyle = cooleurs[0];
+        ctx.fillRect((finalSize / 2 - maxSize / 2) * (1 - theTimeIsNow), (finalSize / 2 - maxSize / 2) * (1 - theTimeIsNow),
+            maxSize + (finalSize - maxSize) * theTimeIsNow, maxSize / 4 + (finalSize - maxSize / 4) * theTimeIsNow);
+        ctx.restore();
+    } else if (t < tEnd) {
+        ctx.fillStyle = "#000";
+        ctx.fillRect(0, 0, finalSize, finalSize);
+
+        let theTimeIsNow = easeInOutCubic((t - tEnd / 4 * 3) / (tEnd / 4));
+
+        ctx.save();
+        ctx.translate(0, finalSize);
+        ctx.rotate(degToRad(theTimeIsNow * 90 - 90));
+        ctx.translate(0, -finalSize);
+
+        ctx.fillStyle = cooleurs[2];
+        ctx.fillRect(0, finalSize / 2, finalSize / 2, finalSize / 4);
+        ctx.fillStyle = cooleurs[3];
+        ctx.fillRect(0, finalSize / 4 * 3, finalSize / 2, finalSize / 4);
+        ctx.restore();
+
+        ctx.save();
+        ctx.translate(finalSize, finalSize);
+        ctx.rotate(degToRad(theTimeIsNow * -90 + 90));
+        ctx.translate(-finalSize, -finalSize);
+
+        ctx.fillStyle = cooleurs[2];
+        ctx.fillRect(finalSize / 2, finalSize / 2, finalSize / 2, finalSize / 4);
+        ctx.fillStyle = cooleurs[3];
+        ctx.fillRect(finalSize / 2, finalSize / 4 * 3, finalSize / 2, finalSize / 4);
+        ctx.restore();
+
+        let upperHalfOffsetY = finalSize / 2 * (1 - theTimeIsNow);
+
+        ctx.fillStyle = cooleurs[0];
+        ctx.fillRect(-upperHalfOffsetY, -upperHalfOffsetY, finalSize / 2, finalSize / 4);
+        ctx.fillStyle = cooleurs[1];
+        ctx.fillRect(-upperHalfOffsetY, -upperHalfOffsetY + finalSize / 4, finalSize / 2, finalSize / 4);
+
+        ctx.fillStyle = cooleurs[0];
+        ctx.fillRect(finalSize / 2 + upperHalfOffsetY, -upperHalfOffsetY, finalSize / 2, finalSize / 4);
+        ctx.fillStyle = cooleurs[1];
+        ctx.fillRect(finalSize / 2 + upperHalfOffsetY, -upperHalfOffsetY + finalSize / 4, finalSize / 2, finalSize / 4);
+    }
+
+    ctx.fillStyle = "#00D4FF";
+    ctx.fillRect(finalSize / 4, finalSize / 4, 2, 2);
+
+    requestAnimationFrame(draw);
+}
+
+function degToRad(deg) {
+    return Math.PI / 180 * deg;
+}
+
+// https://gist.github.com/gre/1650294
+function easeInOutCubic(t) { return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1; }
